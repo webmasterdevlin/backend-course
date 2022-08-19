01f297521c91ffd89bd7342e55442660b5f1034d
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-08-19 14:12:28 +0200

    updated

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 require("dotenv").config(); // for .env file
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const todoModel = require("./todo");
 
 const server = express();
-// parse application/x-www-form-urlencoded
-server.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-server.use(bodyParser.json());
+server.use(express.json());
 
 // req is the request object
 // res is the response object
